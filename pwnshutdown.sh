a064c32a16e83a4b733630102ae18ba0a1cf4bda
makerph <doki@ymail.com>
2020-01-30 15:45:24 +0800

    initial commit
    initial commit

---
+++ pwnshutdown.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This should point to the pivoyager executable
+PIVOYAGER=/usr/local/bin/pivoyager
+
+#  - Enable automatic power on once USB power is restored.
+#  - Enable to powerup rPi every 6:00AM
+#  - Program full power cut in 25 seconds.
+#  - Shutdown rPi
+$PIVOYAGER enable power-wakeup
+$PIVOYAGER alarm *-21-00-00
+$PIVOYAGER watchdog 25
+sudo shutdown now
