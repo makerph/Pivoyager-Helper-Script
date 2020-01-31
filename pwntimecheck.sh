9b52b77944255a4614227d42d2ad647cfd08faf8
makerph <doki@ymail.com>
2020-01-31 15:02:33 +0800

    Update pwntimecheck.sh
    Improve logic and add check for pivoyager clock status before syncing time form pivoyager.

--- pwntimecheck.sh
+++ pwntimecheck.sh
@@ -2,9 +2,13 @@
 # This should point to the pivoyager executable
 PIVOYAGER=/usr/local/bin/pivoyager
 
-  if timedatectl status | grep "System clock synchronized: yes";  then
+  if timedatectl status | grep "System clock synchronized: yes";
+  then
      $PIVOYAGER date sync
-  else
+  elif timedatectl status | grep "System clock synchronized: no" && $PIVOYAGER status | grep "inits";
+  then
      sudo date -s `$PIVOYAGER date`
+  else
+     #echo "update time!"
+	 sudo systemctl restart systemd-timesyncd
   fi
-done
